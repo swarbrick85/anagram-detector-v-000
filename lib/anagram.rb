ea6b5b3580d028c7edd7c958f57148225c02a9e8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-28 13:54:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,37 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(list)
+    a = {}
+    
+    
+    
+    list.each do |wor|
+      b = wor.split("")
+      c = b.sort 
+      h = c.join
+      a[h] = wor 
+    end 
+    
+
+    d = @word.split("")
+    e = d.sort 
+    f = e.join
+    
+    g = [] 
+    a.each do |jumble, wort|
+      if jumble == f 
+        g << wort 
+      end 
+    end 
+    g
+  end 
+  
+end
